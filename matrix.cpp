2f6b84bfb487f062a3761f1931aeab7c66460de9
Timothy Wong <timothy7041@gmail.com>
2025-12-20 00:13:10 -0500

    add operators

--- matrix.cpp
+++ matrix.cpp
@@ -58,6 +58,27 @@ public:
     return trace;
   }
 
+  Matrix3 operator*(const int r) const {
+    Matrix3 scalarMatrix;
+    for (int col = 0; col < MATRIX_SIZE; col++) {
+      for (int row = 0; row < MATRIX_SIZE; row++) {
+        scalarMatrix.value[row][col] = r * value[row][col];
+      }
+    }
+    return scalarMatrix;
+  }
+
+  bool operator==(const Matrix3 &m) const {
+    for (int col = 0; col < MATRIX_SIZE; col++) {
+      for (int row = 0; row < MATRIX_SIZE; row++) {
+        if (m.value[row][col] != value[row][col]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
   void PrintMatrix() {
     for (int col = 0; col < MATRIX_SIZE; col++) {
       for (int row = 0; row < MATRIX_SIZE; row++) {
@@ -79,6 +100,10 @@ public:
   }
 };
 
+Matrix3 operator*(const int lhsScalar, const Matrix3 &rhsMatrix) {
+  return rhsMatrix * lhsScalar;
+}
+
 int main() {
   Matrix3 matrixZero;
   int col1[3] = {1, 2, 3};
@@ -92,6 +117,12 @@ int main() {
   negMatrix.PrintMatrix();
   Matrix3 matrixSub = matrix - matrix;
   matrixSub.PrintMatrix();
-  std::cout << "Trace: " << matrix.Trace() << std::endl;
+  std::cout << "Trace: " << matrix.Trace() << std::endl << std::endl;
+  Matrix3 scalarMatrix1 = matrix * 3;
+  scalarMatrix1.PrintMatrix();
+  Matrix3 scalarMatrix2 = 3 * matrix;
+  scalarMatrix2.PrintMatrix();
+  std::cout << "Equal: " << (scalarMatrix1 == scalarMatrix2) << std::endl;
+  std::cout << "Not Equal: " << (scalarMatrix1 == matrix) << std::endl;
   return 0;
 }
